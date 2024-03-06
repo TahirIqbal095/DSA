1a3682460022943acef31899da45fea591174400
Tahir Iqbal <shahtahir786@gmail.com>
2024-03-06 18:18:31 +0530

    optimization of bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -26,13 +26,16 @@ public class BubbleSort {
         int n = arr.length;
 
         for(int i = n - 1; i >= 1; i--) {
+            int didSwap = 0;
             for(int j = 0; j <= i - 1; j++) {
                 if(arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+ 1];
                     arr[j + 1] = temp;
+                    didSwap = 1;
                 }
             }
+            if(didSwap == 0) break;
         } 
     }
 }
