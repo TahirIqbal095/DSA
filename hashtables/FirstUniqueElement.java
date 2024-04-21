854e0078d79535d473a0bc60ddd61ecffef4dd0e
Tahir Iqbal <shahtahir786@gmail.com>
2024-04-21 08:37:27 +0530

    first unique element

--- hashtables/FirstUniqueElement.java
+++ hashtables/FirstUniqueElement.java
@@ -1,21 +1,49 @@
 package hashtables;
 
+import java.util.HashMap;
+
 public class FirstUniqueElement {
     public static void main(String[] args) {
         String s = "loveleetcode";
-        int c = uniqueEl(s);
+        int c = uniqueElement(s);
         System.out.println(c);
     }
 
+    /**
+     * using hashmap
+     * time complexity : O(n)
+     * space complexity : O(n)
+     */
     public static int uniqueEl(String s) {
-        for (int i = 0; i < s.length() - 1; i++) {
-            int count = 0;
-            char c = s.charAt(i);
-            for (int j = i + 1; j < s.length(); j++) {
-                if (c == s.charAt(j))
-                    count++;
-            }
-            if (count == 0)
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int j = 0; j < s.length(); j++) {
+            if (map.get(s.charAt(j)) == 1)
+                return j;
+        }
+        return -1;
+    }
+
+    /**
+     * using the hashing
+     * Tc - O(n)
+     * Sp - O(26) 26 is the size of array of characters
+     */
+
+    public static int uniqueElement(String s) {
+
+        int[] chars = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            chars[ch - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s.charAt(i) - 'a'] == 1)
                 return i;
         }
         return -1;
--- hashtables/MinimumIndexSum.java
+++ hashtables/MinimumIndexSum.java
@@ -2,7 +2,6 @@ package hashtables;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class MinimumIndexSum {
     public static void main(String[] args) {
